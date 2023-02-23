a7e0c267986795bc3a1ce7072875b10ce832601c
aznale <sergiaznaralemany@gmail.com>
2023-02-23 13:29:50 +0100

    Library Person2

--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -1,8 +1,12 @@
 package org.example;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Person {
     private String name;
     private String surname;
@@ -10,10 +14,11 @@ public class Person {
     private String address;
 
 
-    public Person(String name, String surname, int age, String address) {
+    /*public Person(String name, String surname, int age, String address) {
         this.name = name;
         this.surname = surname;
         this.age = age;
         this.address = address;
-    }
+    }*/
+
 }
