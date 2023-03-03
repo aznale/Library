0b492823d93caf844400b097d4092dbcb7964579
aznale <109067957+aznale@users.noreply.github.com>
2023-03-03 16:38:38 +0100

    Git proof 2

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,30 +1,42 @@
 package org.example;
 
-import org.example.utils.DataValues;
 import org.example.manager.BookManager;
+import org.example.manager.BorrowManager;
 import org.example.manager.UserManager;
 
-import java.util.ArrayList;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
 
-        UserManager.createUsers(4);
+        Scanner reader = new Scanner(System.in);
 
-        System.out.println(DataValues.getIdNumber());
+        System.out.println("----- Users: -----");
+        UserManager.createUsers(5);
+        //System.out.println(DataValues.getIdNumber());
         System.out.println(" ");
 
-        UserManager.createUsers(7);
-
-        System.out.println(DataValues.getIdNumber());
-        System.out.println(" ");
-
+        System.out.println("----- Books: -----");
         BookManager.createBooks(5);
-
         //System.out.println(DataValues.getIdBook());
         System.out.println(" ");
 
-        BookManager.createBooks(2);
+        System.out.println("----- Borrows: -----");
+        BorrowManager.createBorrows(2);
+        System.out.println(" ");
+
+        BorrowManager.borrowStatus();
+        System.out.println(" ");
+
+        //BookManager.createBookToStorage(reader);
+        //BorrowManager.borrowStatus();
+
+        //BookManager.deleteBook(reader);
+        //BorrowManager.borrowStatus();
+
+        //BookManager.updateBook(reader);
+
+        BookManager.getAllBooks();
 
 
 
